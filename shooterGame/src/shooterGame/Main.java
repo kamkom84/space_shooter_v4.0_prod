ae0fb06fce57a5f221f5d1c0348e1955311ca182
kamkom84 <kbd.dimitrov@gmail.com>
2025-09-04 21:46:09 +0300

    cmt

--- shooterGame/src/shooterGame/Main.java
+++ shooterGame/src/shooterGame/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("space_shooter_v4.0_prod");
+            JFrame frame = new JFrame("space_shooter_v.4.0_prod");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
